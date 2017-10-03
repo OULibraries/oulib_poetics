8bfd0638bfa061ee4637077d6a1713cec06ae01c
Caitlin Corbett <ccorbett@ou.edu>
2017-10-03 14:58:48 -0500

    Image caption styling.

--- templates/paragraphs-item--full_width_image_caption.tpl.php
+++ templates/paragraphs-item--full_width_image_caption.tpl.php
@@ -29,7 +29,7 @@
 <div class="paragraph_bundle_full_image_caption row">
 	<div class="col-md-12">
 	<?php print render($content['field_image']); ?>
-	<div class="center"><?php print render($content['field_image_caption']); ?></div>
+	<div class="image-caption"><?php print render($content['field_image_caption']); ?></div>
 	</div>
 </div>
 
